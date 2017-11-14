a84d8737756c4adf2400be2af3ee97a7c704f38e
uno <st9007a@gmail.com>
2017-11-14 09:29:37 +0800

    Read word dictionary in demo

--- demo.py
+++ demo.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+import pickle
 import numpy as np
 from keras.models import load_model, Model
 from keras.layers import Input
 
-full_model = load_model('model/s2s.100.bin')
+word_dict_file = 'data/dict/word_dict.pkl'
+
+# param prepare
+word_dict = None
+with open(word_dict_file, 'rb') as p:
+    word_dict = pickle.load(p)
+
+word_dict_len = len(word_dict)
+
+
+full_model = load_model('model/s2s.70.bin')
 encoder_input = full_model.layers[0]
 encoder = full_model.layers[2]
 decoder_input = full_model.layers[1]
@@ -15,8 +26,8 @@ encoder_state = [state_h, state_c]
 
 encoder_model = Model(encoder_input, encoder_state)
 
-decoder_state_input_h = Input(shape = (3561, ))
-decoder_state_input_c = Input(shape = (3561, ))
+decoder_state_input_h = Input(shape = (word_dict_len, ))
+decoder_state_input_c = Input(shape = (word_dict_len, ))
 decoder_state_input = [decoder_state_input_h, decoder_state_input_c]
 
 decoder_output, state_h, state_c = decoder_lstm(decoder_input, initial_state = decoder_state_input)
@@ -29,8 +40,27 @@ def decode_sequence(input_seq):
 
     state_value = encoder_model.predict(input_seq)
 
-    target_seq = np.zeros((1, 1, 3561))
+    target_seq = np.zeros((1, 1, word_dict_len))
 
-    #TODO: insert one-hot of start_sym to target_seq#
+    #TODO: insert one-hot of start_sym to target_seq
 
+    stop_condition = False
+    decode_sentence = ''
 
+    while not stop_condition:
+        output_token, h, c = decoder_model.predict([target_seq] + state_value)
+
+        sample_word = None
+        # TODO: sample word from output_token, and concat with decode_sentence
+        decode_sentence += sample_word
+
+        if sample_word == end_sym or len(decode_sentence) >= 15:
+            stop_condition = True
+
+        target_seq = np.zeros((1, 1, word_dict_len))
+
+        #TODO: insert one-hot of start_sym to target_seq
+
+        state_value = [h, c]
+
+    return decode_sentence
